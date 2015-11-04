5f0a9a70264201e377d4885e35592c9bafbe1640
Echo19900318 <zyj3182010@gmail.com>
2015-11-04 14:30:53 +0800

    tyuio

---
+++ TestingGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingGit
+//
+//  Created by THV1WP15S on 4/11/15.
+//  Copyright (c) 2015 Stiky Hive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestingGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestingGit
+//
+//  Created by THV1WP15S on 4/11/15.
+//  Copyright (c) 2015 Stiky Hive. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
